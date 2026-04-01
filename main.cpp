51c49e218f80cc64ee9e23f11fc9561a01431d5d
meats88 <pcx883mi@gmail.com>
2026-03-31 18:32:36 -0700

    Fix weight and distance validation messages

--- main.cpp
+++ main.cpp
@@ -14,17 +14,17 @@ int main()
     cout << "Enter the package weight and distance: ";
     cin >> weight >> distance;
 if (weight <= 0 || weight > 20){
-    cout << "weight wrong" << endl;
+    cout << "weight wrong" << endl;}
     else if (distance < 10 || distance > 3000){
-        cout << distance wrong << endl;
+        cout << "distance wrong" << endl;
     }
     else {
         if (weigth <= 2){rate = rate1;}
-        else if (weight <= 6){rate = rate 2;}
+        else if (weight <= 6){rate = rate2;}
         else if (weight <= 10){rate = rate3;}
-        else {rate = rate4;}}
+        else {rate = rate4;}
     total_charge = (distance / 500) * rate;
-        
+    }
     
     // TODO
 
